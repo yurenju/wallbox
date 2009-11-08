82655a60d4221565dcd0c4b52110b9bdaf92c9bf
Yuren Ju <yurenju@gmail.com>
2009-11-08 19:25:50 +0000

    finish postoffice dbus test

--- tests/post_office_test.py
+++ tests/post_office_test.py
@@ -15,6 +15,7 @@ class TestDbusPostOffice (unittest.TestCase):
             (obj, "org.wallbox.PostOfficeInterface")
 
         self.login ()
+        time.sleep (5)
 
     def get_notification_test (self):
         while self.office.get_office_status () != wallbox.STANDBY:
@@ -25,8 +26,26 @@ class TestDbusPostOffice (unittest.TestCase):
             self.assert_ ("test" in n['body_text'])
 
     def post_status_test (self):
-        
+        text1 = "post test status"
+        self.office.post_status (text)
+        self.office.refresh ()
+        self.sleep (5)
+        status = self.get_current_status ()
+        self.assert_ (text1 == status['message'])
 
+    def comment_test (self):
+        text1 = "post test comment"
+
+        status = self.office.get_current_status ()
+        post_id = "%d_%s" % (status['uid'], status['status_id'])
+
+        self.office.post_comment (post_id)
+        time.sleep (5)
+        status = self.office.get_current_status ()
+
+        comments = self.office.get_comments (post_id)
+        c = comments.pop ()
+        self.assert_ (c['text'] == text1)
 
 if __name__ == "__main__":
     unittest.main ()

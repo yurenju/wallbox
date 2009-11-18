8e8c5570065d703e50c915ce0b01574c87c6400e
Yuren Ju <yurenju@gmail.com>
2009-11-18 23:55:25 +0000

    add close feature after press apply

--- data/wizard.py
+++ data/wizard.py
@@ -12,6 +12,7 @@ class Wizard:
         self.assistant = self.builder.get_object ("wizard_welcome")
         self.assistant.set_page_complete (self.assistant.get_nth_page (0), True)
         self.assistant.set_page_complete (self.assistant.get_nth_page (3), True)
+        self.assistant.connect ("apply", self.apply, None)
         self.assistant.show ()
 
         self.builder.connect_signals (self)
@@ -23,13 +24,18 @@ class Wizard:
         self.office = dbus.Interface \
             (obj, "org.wallbox.PostOfficeInterface")
 
+    def apply (self, widget, data=None):
+        self.assistant.destroy ()
+
     def show_continue_button (self, remove_widget, page_index, box_name):
         hbox = self.builder.get_object (box_name)
         hbox.remove (remove_widget)
         continue_button = gtk.Button ("Continue")
         hbox.pack_start (continue_button, True, False, 0)
         continue_button.show ()
-        self.continue_id = continue_button.connect ("clicked", self.on_button_continue_clicked, page_index)
+        self.continue_id = \
+            continue_button.connect \
+            ("clicked", self.on_button_continue_clicked, page_index)
 
     def on_button_extra_perm_clicked (self, widget, data=None):
         self.office.get_ext_perm ()
@@ -40,7 +46,9 @@ class Wizard:
         self.show_continue_button (widget, 1, "hbox_active")
 
     def on_button_continue_clicked (self, widget, data=None):
-        self.assistant.set_page_complete (self.assistant.get_nth_page (int (data)), True)
+        self.assistant.set_page_complete \
+            (self.assistant.get_nth_page (int (data)), True)
+
         self.assistant.set_current_page (int(data)+1)
         widget.disconnect (self.continue_id)
 

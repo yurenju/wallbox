e217bd3f2adb2c2ebc57548910aa91ffe8f6ad10
Yuren Ju <yurenju@gmail.com>
2009-11-23 08:10:08 +0000

    add refresh method

--- data/notification.py
+++ data/notification.py
@@ -19,6 +19,18 @@ class NotificationView:
         self.office = dbus.Interface \
             (obj, "org.wallbox.PostOfficeInterface")
 
+        self.office.refresh (reply_handler=self.refresh_reply_cb, \
+                            error_handler=self.refresh_error_cb)
+
+    def refresh_reply_cb (self):
+        nlist = self.office.get_notification_list ()
+        liststore = self.builder.get_object ("list_notification")
+        for nid in nlist:
+            entry = self.office.get_notification_entry (nid)
+            liststore.append ([None, entry[''], False])
+
+    def refresh_error_cb (self, e):
+        pass
 
 if __name__ == "__main__":
     n = NotificationView ()

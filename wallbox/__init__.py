cb1e8e621a455e43abaa4da291abf9e01f9fb0a4
Yuren Ju <yurenju@gmail.com>
2009-12-20 15:51:57 +0000

    add thread support

--- wallbox/__init__.py
+++ wallbox/__init__.py
@@ -13,6 +13,8 @@ import time
 import re
 import datetime
 import secert
+import threading
+import gtk
 
 __author__ = 'Yuren Ju <yurenju@gmail.com>'
 
@@ -20,6 +22,184 @@ IS_LOGIN = 0
 REFRESHING = 1
 WAITING_LOGIN = 2
 NO_LOGIN = 3
+
+gtk.gdk.threads_init()
+
+class RefreshProcess (threading.Thread):
+    def __init__ (self, notification_num, fb, uid, user_icons_dir, app_icons_dir, user_ids):
+        threading.Thread.__init__ (self)
+        self.uid = uid
+        self.notification_num = notification_num
+        self.fb = fb
+        self.current_status = None
+        self.app_ids = []
+        self.applications = {}
+        self.user_ids = user_ids
+        self.users = []
+        self.status = {}
+        self.updated_timestamp = None
+        self.refresh_interval = 60
+        self.notification = []
+        self.user_icons_dir = user_icons_dir
+        self.app_icons_dir = app_icons_dir
+    
+    def _dump_comments (self, post_id):
+        print "status: %s: %s" % (post_id, self.status[post_id]['message'])
+        print "comments:"
+        for c in self.status[post_id]['comments']:
+            print "\t%s" % c['text']
+
+    def _dump_status (self):
+        print "=== START === dump status"
+        for skey in self.status:
+            if self.status[skey].has_key ('message'):
+                print "status: %s" % self.status[skey]['message']
+            else:
+                print "ERROR status key: %s has no message" % skey
+                print "detail:\n%s" % self.status[skey]
+            if not self.status[skey].has_key ('notification_ids'):
+                print "NO notification_ids"
+                continue
+            print "nids: ",
+            for n in self.status[skey]['notification_ids']:
+                print "%s, " % n,
+            print "\n=== END === dump status"
+
+    def _query (self, query_str):
+        for i in range (3):
+            try:
+                result = self.fb.fql.query (query_str)
+                if result != None:
+                    return result
+            except:
+                print "URLError, Sleep 3 sec"
+                time.sleep (3)
+        return None
+            
+    def get_remote_current_status (self):
+        print "get remote current status"
+        qstr = "SELECT uid, status_id, message, source FROM status WHERE uid='%s' LIMIT 1" % self.uid
+        status = self._query (qstr)
+
+        self.current_status = status[0]
+
+    def get_remote_notification (self):
+        print "get remote notification"
+        notification = self._query \
+            ("SELECT notification_id, title_text, body_text, is_unread, " + \
+            "is_hidden, href, app_id, sender_id " + \
+            "FROM notification WHERE recipient_id = '%s' LIMIT %s" % \
+            (self.uid, self.notification_num))
+        self.notification = notification
+
+    def get_remote_comments (self):
+        print "get remote comments"
+        pattern_id = re.compile ("&id=(\d+)")
+        pattern_fbid = re.compile ("story_fbid=(\d+)")
+        delete_n= []
+        new_status = {}
+        for n in self.notification:
+            print "app_id: %s: %s" % (n['app_id'], n['body_text'])
+            if n['app_id'] == 19675640871:
+                #get post_id
+                post_id = None
+                m_id = pattern_id.search (n['href'])
+                m_fbid = pattern_fbid.search (n['href'])
+
+                if m_id != None and m_fbid != None:
+                    id = m_id.group (1)
+                    status_id = m_fbid.group (1)
+                    post_id = "%s_%s" % (id, status_id)
+                    if not new_status.has_key (post_id):
+                        new_status[post_id] = {}
+                        qstr = "SELECT uid, time, message, status_id FROM status " + \
+                            "WHERE uid = %s AND status_id = %s LIMIT 1" % \
+                            (id, status_id)
+                        result = self._query (qstr)
+
+                        if result != None and len (result) > 0:
+                            new_status[post_id] = result[0]
+                        else:
+                            print "can't get status_id"
+                            print "detail\n===\nbody: %s\nhref: %s\nquery str: %s\n===" % \
+                                (n['body_text'], n['href'], qstr)
+                            delete_n.append (n)
+                            del new_status[post_id]
+                            continue
+
+                        qstr = "SELECT id, fromid, text, time FROM comment WHERE post_id='%s'" % post_id
+                        comments = self._query (qstr)
+
+                        new_status[post_id]['comments'] = comments
+                        #self._dump_comments (post_id, new_status)
+                    if not new_status[post_id].has_key ('notification_ids'):
+                        new_status[post_id]['notification_ids'] = []
+                    if not int (n['notification_id']) in new_status[post_id]['notification_ids']:
+                        new_status[post_id]['notification_ids'].append (int (n['notification_id']))
+        self.status = new_status
+        for n in delete_n:
+            print "delete notification: %s" % n['body_text']
+            ni = self.notification.index (n)
+            del self.notification[ni]
+        self._dump_status ()
+
+    def get_remote_users_icon (self):
+        print "get remote users icon"
+        for n in self.notification:
+            if not n['sender_id'] in self.user_ids:
+                self.user_ids.append (n['sender_id'])
+        for skey in self.status:
+            if self.status[skey].has_key ('comments'):
+                for c in self.status[skey]['comments']:
+                    if not c['fromid'] in self.user_ids:
+                        self.user_ids.append (c['fromid'])
+        self.users = \
+            self.fb.users.getInfo (self.user_ids, ["name", "pic_square"])
+
+
+        for u in self.users:
+            if (len (u['pic_square']) > 0):
+                icon_name = \
+                    os.path.basename \
+                    (urlparse.urlsplit (u['pic_square']).path)
+
+                urllib.urlretrieve \
+                    (u['pic_square'], "%s/%s" % \
+                    (self.user_icons_dir, icon_name))
+                u['pic_square_local'] = icon_name
+
+    def get_remote_applications_icon (self):
+        print "get remote applications icon"
+        for n in self.notification:
+            if not n['app_id'] in self.app_ids:
+                self.app_ids.append (n['app_id'])
+
+        for app_id in self.app_ids:
+            app = self.fb.fql.query \
+                ("SELECT icon_url FROM application WHERE app_id='%d'" % \
+                app_id)[0]
+
+            icon_name = os.path.basename \
+                (urlparse.urlsplit (app['icon_url']).path)
+                
+            urllib.urlretrieve \
+                (app['icon_url'], "%s/%s" % (self.app_icons_dir, icon_name))
+            self.applications[app_id] = {'icon_name': icon_name}
+
+    def run (self):
+        self.get_remote_current_status ()
+        time.sleep (2)
+        self.get_remote_notification ()
+        time.sleep (2)
+        self.get_remote_comments ()
+        time.sleep (2)
+        self.get_remote_users_icon ()
+        time.sleep (2)
+        self.get_remote_applications_icon ()
+        time.sleep (2)
+        self.updated_timestamp = datetime.date.today ()
+        print "updated finish"
+
 
 class PostOffice (dbus.service.Object):
     def __init__ (self, bus_name, bus_path):
@@ -92,6 +272,8 @@ class PostOffice (dbus.service.Object):
         for n in self.notification:
             if int (n['notification_id']) == notification_id:
                 return n
+        else:
+            return {}
 
     @dbus.service.method ("org.wallbox.PostOfficeInterface", in_signature='ss', out_signature='')
     def post_comment (self, post_id, comment):
@@ -124,27 +306,35 @@ class PostOffice (dbus.service.Object):
                 return c
         return {}
 
+    def check_refresh_complete (self):
+        if self.rs.is_alive ():
+            return True
+
+        self.current_status = self.rs.current_status
+        self.notification = self.rs.notification
+        self.status = self.rs.status
+        self.user_ids = self.rs.user_ids
+        self.users = self.rs.users
+        self.app_ids = self.rs.app_ids
+        self.applications = self.rs.applications
+
+        self.status_changed (self.orig_office_status)
+        return False
+
     @dbus.service.method ("org.wallbox.PostOfficeInterface", in_signature='', out_signature='')
     def refresh (self):
         if self.office_status != IS_LOGIN:
             print "not IS_LOGIN"
             return True
-        print "refresh start"
-        orig_status = self.office_status
+
+        self.orig_office_status = self.office_status
         self.status_changed (REFRESHING)
-        self.get_remote_current_status ()
-        time.sleep (2)
-        self.get_remote_notification ()
-        time.sleep (2)
-        self.get_remote_comments ()
-        time.sleep (2)
-        self.get_remote_users_icon ()
-        time.sleep (2)
-        self.get_remote_applications_icon ()
-        time.sleep (2)
-        self.updated_timestamp = datetime.date.today ()
-        print "updated finish"
-        self.status_changed (orig_status)
+        print "notification_num: %s" % self.notification_num
+        self.rs = RefreshProcess \
+            (self.notification_num, self.fb, self.uid, self.user_icons_dir, self.app_icons_dir, self.user_ids)
+        self.rs.start ()
+        gobject.timeout_add (1000, self.check_refresh_complete)
+
         return True
 
     @dbus.service.method ("org.wallbox.PostOfficeInterface", in_signature='s', out_signature='')
@@ -155,13 +345,18 @@ class PostOffice (dbus.service.Object):
     def get_current_status (self):
         print "get current status"
         print "%s\n" % self.current_status['message']
+        if len (self.current_status) < 1:
+            return {}
         return self.current_status
 
     @dbus.service.method ("org.wallbox.PostOfficeInterface", in_signature='s', out_signature='a{sv}')
     def get_status (self, post_id):
         result = self.status[post_id].copy ()
+        print result
         if result.has_key ('comments'):
             del result['comments']
+        if len (result) < 1:
+            return {}
         return result
 
     @dbus.service.method ("org.wallbox.PostOfficeInterface", in_signature='i', out_signature='a{sv}')
@@ -193,12 +388,15 @@ class PostOffice (dbus.service.Object):
         for u in self.users:
             if u['uid'] == self.uid:
                 return u
+        print "no current user"
+        return {}
 
     @dbus.service.method ("org.wallbox.PostOfficeInterface", in_signature='x', out_signature='a{sv}')
     def get_user (self, uid):
         for u in self.users:
             if u['uid'] == uid:
                 return u
+        return {}
 
     @dbus.service.method ("org.wallbox.PostOfficeInterface", in_signature='', out_signature='s')
     def get_app_icons_dir (self):
@@ -212,147 +410,6 @@ class PostOffice (dbus.service.Object):
     def status_changed (self, status):
         self.office_status = status
         print "emit signal: %s" % status
-
-    def get_remote_current_status (self):
-        print "get remote current status"
-        qstr = "SELECT uid, status_id, message, source FROM status WHERE uid='%s' LIMIT 1" % self.uid
-        status = self._query (qstr)
-
-        self.current_status = status[0]
-
-    def get_remote_notification (self):
-        print "get remote notification"
-        notification = self._query \
-            ("SELECT notification_id, title_text, body_text, is_unread, " + \
-            "is_hidden, href, app_id, sender_id " + \
-            "FROM notification WHERE recipient_id = '%s' LIMIT %s" % \
-            (self.uid, self.notification_num))
-        self.notification = notification
-
-    def _dump_comments (self, post_id):
-        print "status: %s: %s" % (post_id, self.status[post_id]['message'])
-        print "comments:"
-        for c in self.status[post_id]['comments']:
-            print "\t%s" % c['text']
-
-    def _dump_status (self):
-        print "=== START === dump status"
-        for skey in self.status:
-            if self.status[skey].has_key ('message'):
-                print "status: %s" % self.status[skey]['message']
-            else:
-                print "ERROR status key: %s has no message" % skey
-                print "detail:\n%s" % self.status[skey]
-            if not self.status[skey].has_key ('notification_ids'):
-                print "NO notification_ids"
-                continue
-            print "nids: ",
-            for n in self.status[skey]['notification_ids']:
-                print "%s, " % n,
-            print "\n=== END === dump status"
-
-    def _query (self, query_str):
-        for i in range (3):
-            try:
-                result = self.fb.fql.query (query_str)
-                if result != None:
-                    return result
-            except:
-                print "URLError, Sleep 3 sec"
-                time.sleep (3)
-        return None
-            
-    def get_remote_comments (self):
-        print "get remote comments"
-        pattern_id = re.compile ("&id=(\d+)")
-        pattern_fbid = re.compile ("story_fbid=(\d+)")
-        delete_n= []
-        new_status = {}
-        for n in self.notification:
-            print "app_id: %s: %s" % (n['app_id'], n['body_text'])
-            if n['app_id'] == 19675640871:
-                #get post_id
-                post_id = None
-                m_id = pattern_id.search (n['href'])
-                m_fbid = pattern_fbid.search (n['href'])
-
-                if m_id != None and m_fbid != None:
-                    id = m_id.group (1)
-                    status_id = m_fbid.group (1)
-                    post_id = "%s_%s" % (id, status_id)
-                    if not new_status.has_key (post_id):
-                        new_status[post_id] = {}
-                        qstr = "SELECT uid, time, message, status_id FROM status " + \
-                            "WHERE uid = %s AND status_id = %s LIMIT 1" % \
-                            (id, status_id)
-                        result = self._query (qstr)
-
-                        if result != None and len (result) > 0:
-                            new_status[post_id] = result[0]
-                        else:
-                            print "can't get status_id"
-                            print "detail\n===\nbody: %s\nhref: %s\nquery str: %s\n===" % (n['body_text'], n['href'], qstr)
-                            delete_n.append (n)
-                            del new_status[post_id]
-                            continue
-
-                        qstr = "SELECT id, fromid, text, time FROM comment WHERE post_id='%s'" % post_id
-                        comments = self._query (qstr)
-
-                        new_status[post_id]['comments'] = comments
-                        #self._dump_comments (post_id, new_status)
-                    if not new_status[post_id].has_key ('notification_ids'):
-                        new_status[post_id]['notification_ids'] = []
-                    if not int (n['notification_id']) in new_status[post_id]['notification_ids']:
-                        new_status[post_id]['notification_ids'].append (int (n['notification_id']))
-        self.status = new_status
-        for n in delete_n:
-            print "delete notification: %s" % n['body_text']
-            ni = self.notification.index (n)
-            del self.notification[ni]
-        self._dump_status ()
-
-    def get_remote_users_icon (self):
-        print "get remote users icon"
-        for n in self.notification:
-            if not n['sender_id'] in self.user_ids:
-                self.user_ids.append (n['sender_id'])
-        for skey in self.status:
-            if self.status[skey].has_key ('comments'):
-                for c in self.status[skey]['comments']:
-                    if not c['fromid'] in self.user_ids:
-                        self.user_ids.append (c['fromid'])
-        self.users = \
-            self.fb.users.getInfo (self.user_ids, ["name", "pic_square"])
-
-        for u in self.users:
-            if (len (u['pic_square']) > 0):
-                icon_name = \
-                    os.path.basename \
-                    (urlparse.urlsplit (u['pic_square']).path)
-
-                urllib.urlretrieve \
-                    (u['pic_square'], "%s/%s" % \
-                    (self.user_icons_dir, icon_name))
-                u['pic_square_local'] = icon_name
-
-    def get_remote_applications_icon (self):
-        print "get remote applications icon"
-        for n in self.notification:
-            if not n['app_id'] in self.app_ids:
-                self.app_ids.append (n['app_id'])
-
-        for app_id in self.app_ids:
-            app = self.fb.fql.query \
-                ("SELECT icon_url FROM application WHERE app_id='%d'" % \
-                app_id)[0]
-
-            icon_name = os.path.basename \
-                (urlparse.urlsplit (app['icon_url']).path)
-                
-            urllib.urlretrieve \
-                (app['icon_url'], "%s/%s" % (self.app_icons_dir, icon_name))
-            self.applications[app_id] = {'icon_name': icon_name}
 
     def prepare_directories (self):
         self.user_icons_dir = \

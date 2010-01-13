cb666cae8e4d2fb233098058365daa4a11f8014d
Yuren Ju <yurenju@gmail.com>
2010-01-13 20:38:38 +0800

    move pickle functions to utils

--- src/wallbox/utils.py
+++ src/wallbox/utils.py
@@ -5,8 +5,14 @@ pygtk.require ("2.0")
 import gtk
 import defs
 import logging
+import pickle
+import os
+import facebook
 
 logging.basicConfig (level=defs.log_level)
+cache_attributes = \
+    ["current_status", "notification", "status", \
+    "user_ids", "users", "app_ids", "applications"]
 
 def set_scollbar_height (window, treeview, scrollbar):
     (width, height) = window.get_size ()
@@ -19,3 +25,58 @@ def set_scollbar_height (window, treeview, scrollbar):
     else:
         scrollbar.set_size_request (-1, treeview_height)
         logging.info ("notification height: %d" % treeview_height)
+
+def pickle_load (path):
+    logging.debug ("pickle_load path: %s" % path)
+    cache = None
+    if os.path.exists (path):
+        f = open (path, 'r')
+        try:
+            cache = pickle.load (f)
+        except:
+            logging.debug ("load cache pickle failed")
+            return
+        f.close ()
+        return cache
+    else:
+        logging.debug ("cache.pickle not found")
+
+    return None
+
+def save_auth_status (path, session):
+    auth = {}
+    auth["session_key"] = session['session_key']
+    auth["secret"] = session['secret']
+    output = open (path, 'wb')
+    pickle.dump (auth, output)
+    output.close ()
+
+def restore_auth_status (path, api_key, secret):
+    if os.path.exists (path):
+        f = open (path, 'r')
+        try:
+            auth = pickle.load (f)
+        except:
+            f.close ()
+            logging.debug ("pickle load failed")
+            return False
+
+        f.close ()
+        fb = facebook.Facebook (api_key, secret)
+        fb.session_key = auth["session_key"]
+        fb.uid = auth["session_key"].split('-')[1]
+        fb.secret = auth["secret"]
+        return fb
+    else:
+        "pickle auth not exist"
+        return None
+
+def pickle_dump (post_office, path):
+    cache = {}
+    for attr_str in cache_attributes:
+        attr = getattr (post_office, attr_str)
+        cache[attr_str] = attr
+    output = open (path, 'wb')
+    pickle.dump (cache, output)
+    output.close ()
+

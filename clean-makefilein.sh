28d4a375604a2eed13a62abd61f7bd3cf2c6120c
Yuren Ju <yurenju@gmail.com>
2010-02-14 20:51:08 +0800

    add defs.py function

---
+++ clean-makefilein.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm Makefile.in
+rm src/Makefile.in
+rm src/wallbox/Makefile.in
+rm data/Makefile.in
+
+touch Makefile.in
+touch src/Makefile.in
+touch src/wallbox/Makefile.in
+touch data/Makefile.in
+
